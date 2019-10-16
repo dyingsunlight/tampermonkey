6d1cb9cd1656e7b9d21881824c7b21a06b495f86
Dogfish <dyingsunlight@gmail.com>
2019-10-16 21:11:27 +0800

    add fuck zhihu zhuanlan script

---
+++ scripts/fuck-zhihu-zhuanlan.js
@@ -0,0 +1,52 @@
+// ==UserScript==
+// @name         fuck-zhihu-zhuanlan
+// @namespace    https://github.com/dyingsunlight/tampermonkey
+// @version      0.1
+// @description  Fuck off Zhihu Zhuanlan, base on MutationObserver
+// @author       Dogfish
+// @match        https://www.zhihu.com/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  'use strict';
+  (new MutationObserver(mutations => mutations.forEach(blockExecutor))).observe(document, {
+    childList: true,
+    subtree: true,
+    characterData: false,
+    attributes: false
+  });
+  const checkedMarkClass = '.zhuanlan-checked'
+  function blockExecutor() {
+    const elements = document.querySelectorAll(`a[href*="zhuanlan.zhihu.com"]:not(${checkedMarkClass})`)
+    for (let element of elements) {
+      element.classList.add(checkedMarkClass)
+      const cardElement = findParentElementUntilMeetClass(element, 'Card')
+      if (!cardElement) {
+        console.log('cardElement not found',  cardElement, element)
+        continue
+      }
+      cardElement.style.display = 'none!important'
+    }
+  }
+  // Utils
+  function findParentElementUntilMeetClass(element, classes) {
+    
+    const classList = typeof classes === 'string' ? classes.split(' ') : classes
+    if (!Array.isArray(classList)) {
+      throw new Error('Classes Must be an array or string!')
+    }
+    
+    let next = element
+    while (next) {
+      if (classList.some(val => next.classList.contains(val))) {
+        return next
+      }
+      if (next.parentElement) {
+        next = next.parentElement
+      } else {
+        return
+      }
+    }
+  }
+})();
